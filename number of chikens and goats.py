58614234bcd6deabd501d4ab702a621214a91c01
samarthlawania <93425520+samarthlawania@users.noreply.github.com>
2021-11-21 22:49:45 +0530

    Add files via upload

---
+++ number of chikens and goats.py
@@ -0,0 +1,5 @@
+F = int(input("Enter the number of feet"))
+H = int(input("Enter the number of heads"))
+C = ((4 * H) - F) / 2
+G = H - C
+print(f"The number of chikens in a farm is {C} \nThe number of goats in a farm is {G}")
